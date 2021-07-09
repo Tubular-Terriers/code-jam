e4e092d6224129c89f8e8f55ed232a8e67431144
Lint Action <lint-action@samuelmeuli.com>
2021-07-09 21:01:50 +0000

    Fix code style issues with Black

--- badpython.py
+++ badpython.py
@@ -1,6 +1,7 @@
 # IMPORTANT ################
 from curses import wrapper
 import curses
+
 stdscr = curses.initscr()
 #############################
 
@@ -10,12 +11,12 @@ def main(stdscr):
     stdscr.clear()
 
     # This raises ZeroDivisionError when i == 10.
-    for i        in    range(0, 11):
+    for i in range(0, 11):
         v = i - 10
-        stdscr       .addstr(i, 0, '10 divided by {} is {}'.format(v, 10/v))
+        stdscr.addstr(i, 0, "10 divided by {} is {}".format(v, 10 / v))
 
-    stdscr  .refresh()
-    stdscr    .getkey()
+    stdscr.refresh()
+    stdscr.getkey()
 
 
-wrapper(   main   )
+wrapper(main)
