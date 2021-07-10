92409a5d23623a2186e800059a682d259e8f5a3d
nopeless <38830903+nopeless@users.noreply.github.com>
2021-07-11 06:35:02 +0900

    add server side debugging renderer

---
+++ src/game/render/colors.py
@@ -0,0 +1,4 @@
+# Write color values for rendering here
+# Use rgb or hex value parser so it meets pygame's rgb requirements
+
+PLAYER = "BLACK"  # for example
---
+++ src/game/render/main.py
@@ -0,0 +1,72 @@
+# a pygame renderer
+import asyncio
+
+import pygame
+import pymunk
+import pymunk.pygame_util
+
+from . import colors
+
+# FIXME while I intent to make this module an instance based application,
+# It seems that I need to use the threading module to do that
+# For now, only one pygame instance can run
+
+
+class Renderer:
+    """
+    Creates instance of renderer
+
+    Parameters:
+        space (pymunk.Space): the space to render
+    """
+
+    def __init__(self, space, quiet=False):
+        # TODO allow this 600 to be a variable
+        self.w = 600
+        self.h = 600
+        self.space = space
+        self.coroutine = None
+        # self.loop = asyncio.get_event_loop()
+        if not quiet:
+            self.init_pygame()
+
+    async def update_loop(self):
+        while True:
+            print("rendering")
+            self.update()
+            # TODO  find a way to sync this with game tick rate
+            await asyncio.sleep(1 / 10)
+
+    def init_pygame(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((self.w, self.h))
+
+        # TODO Temporary debug code
+        self.draw_options = pymunk.pygame_util.DrawOptions(self.screen)
+        # self.coroutine = self.loop.create_task(self.update_loop())
+        # self.loop.run_until_complete(self.coroutine)
+
+        # self.run
+
+    # TODO implement this later
+    def run(self):
+        while self.running:
+            # Check for quit
+            for event in pygame.event.get():
+                if (
+                    event.type == pygame.QUIT
+                    or event.type == pygame.KEYDOWN
+                    and event.key == pygame.K_ESCAPE
+                ):
+                    self.running = False
+
+            # TODO someone make this rgb
+            self.screen.fill(pygame.Color("white"))
+
+    def update(self):
+        for event in pygame.event.get():
+            pass
+        self.screen.fill("GRAY")
+        self.space.debug_draw(self.draw_options)
+        pygame.display.update()
+        self.space.step(0.01)
