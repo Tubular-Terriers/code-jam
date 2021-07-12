643c2184ae5bc489b2622b4e0c38fea08996e040
nopeless <38830903+nopeless@users.noreply.github.com>
2021-07-12 20:51:24 +0900

    ui for menu

--- src/client/ui/menu.py
+++ src/client/ui/menu.py
@@ -1,4 +1,29 @@
-# this is an example
+import asyncio
+import time
+from time import sleep
 
-# should return a menu object
-from .UI import UI
+from appstate import AppState
+from ui.UI import UI
+
+from .widget.loading_bar import LoadingBar
+
+
+class Menu(UI):
+    def __init__(self):
+        super().__init__("menu screen")
+        self.time = 0
+
+    async def view(self, app):
+        # Required
+        super().view(app)
+        self.time += 10
+        my_bar = LoadingBar(width=32, y=3, x=3, message="hello")
+        my_bar.set_progress(self.time)
+        self.widgets = [my_bar]
+        self.window.addstr(0, 0, "I am a menu")
+        self.refresh()
+        return AppState.MENU
+
+
+# Return single menu object
+menu = Menu()
