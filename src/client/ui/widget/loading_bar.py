6864a81e0256ec544de98fc3f942c19642aab04c
madara uchiha <pritanshsahsani@gmail.com>
2021-07-12 12:20:35 +0530

    Added health bar and debugged loading bar

--- src/client/ui/widget/loading_bar.py
+++ src/client/ui/widget/loading_bar.py
@@ -5,34 +5,31 @@ curses.initscr()
 
 
 class loading_bar:
-    def __init__(self, width, y, x, message=None):
-        self.height = 3
+    def __init__(self, width, y, x, message="lol"):
+        self.height = 5
         self.width = width
         self.y = y
         self.x = x
         self.message = message
         self.win = curses.newwin(self.height, self.width, self.y, self.x)
-        self.win.border(0)
 
     def update(self):
-        if self.message is not None:
-            self.win.addstr(1, 1, self.message)
+        self.win.erase() 
         x = int(self.progress)
-        x = x * ((self.width - 2) / float(100))
+        x = x/10
         x = int(x)
-        display = "#"
+        display =self.message+"\n┌──────────┐\n│"+"#"*x+"-"*(10-x)+"│\n└──────────┘"
         if x != 0:
-            self.win.addstr(1, x, f"{display}")
+            self.win.addstr(0, 1, f"{display}")
             self.win.refresh()
 
     def set_progress(self, progress: float):
         self.progress = progress
         self.update()
 
-
-a = loading_bar(width=32, y=0, x=0)
+a = loading_bar( 32, 3, 30, "nani") 
 loading = 0
 while loading < 100:
     loading += 1
-    time.sleep(0.03)
     a.set_progress(loading)
+    time.sleep(0.03)
