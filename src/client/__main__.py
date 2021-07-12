1a842289a24d8cf53d3c54e62821b2a79c9a2e22
nopeless <38830903+nopeless@users.noreply.github.com>
2021-07-12 20:44:56 +0900

    add main invoker

--- src/client/__main__.py
+++ src/client/__main__.py
@@ -1,4 +1,17 @@
 # write argument parser here
 
-
 # starts a ui
+
+import asyncio
+
+from app import app
+
+# Someone set up a logger
+# import logging
+
+
+async def main():
+    await app.run()
+
+
+asyncio.run(main())
