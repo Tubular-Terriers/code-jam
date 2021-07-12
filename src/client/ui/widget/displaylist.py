77a81de6203e2a0a23f9d5150860e5b7dbf816f8
Lint Action <lint-action@samuelmeuli.com>
2021-07-12 03:56:56 +0000

    Fix code style issues with Black

--- src/client/ui/widget/displaylist.py
+++ src/client/ui/widget/displaylist.py
@@ -16,26 +16,30 @@ class Listdisplay:
         self.pad = curses.newpad(1 + len(lst) + 1, width)
 
         for n in range(len(lst)):
-            self.pad.addstr(n+1, 1, lst[n])
+            self.pad.addstr(n + 1, 1, lst[n])
         self.pad.border(0)
         self.pad.refresh(self.currentpos, 0, start_x, start_y, height, width)
 
     def scrollup(self):
         self.currentpos += 1
-        self.currentpos = min(len(self.lst)-self.height+1, self.currentpos)
-        self.pad.refresh(self.currentpos, 0, self.start_x, self.start_y, self.height, self.width)
+        self.currentpos = min(len(self.lst) - self.height + 1, self.currentpos)
+        self.pad.refresh(
+            self.currentpos, 0, self.start_x, self.start_y, self.height, self.width
+        )
 
     def scrolldown(self):
         self.currentpos -= 1
         self.currentpos = max(0, self.currentpos)
-        self.pad.refresh(self.currentpos, 0, self.start_x, self.start_y, self.height, self.width)
+        self.pad.refresh(
+            self.currentpos, 0, self.start_x, self.start_y, self.height, self.width
+        )
 
     def switchtab(self, diff):
         pass
 
 
 def main(stdscr):
-    lst = ['hii', 'hello', 'dkfjalkf', 'lajdflkdf']    # for debugging
+    lst = ["hii", "hello", "dkfjalkf", "lajdflkdf"]  # for debugging
     stdscr.clear()
     a = Listdisplay(lst, 0, 0, 3, 10)
     time.sleep(2)
