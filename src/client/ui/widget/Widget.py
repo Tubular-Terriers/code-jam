09d7bcd3ac79f5a5c46878c4fce1391635af1979
MePhew <basmateusz@wp.pl>
2021-07-12 17:24:31 +0200

    Rename widget base class file

--- src/client/ui/widget/Widget.py
+++
@@ -1,9 +0,0 @@
-# The base class for all widgets
-
-
-class Widget:
-    def __init__(self, name):
-        pass
-
-    def key_press_on(self, key):
-        pass
---
+++ src/client/ui/widget/_widget.py
@@ -0,0 +1,9 @@
+# The base class for all widgets
+
+
+class Widget:
+    def __init__(self, name):
+        pass
+
+    def key_press_on(self, key):
+        pass
