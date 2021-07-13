74179a275e9326064d289db1553624f85b4e2d52
MePhew <basmateusz@wp.pl>
2021-07-13 16:02:12 +0200

    Improve sound files storage structure

--- src/client/sound/sounds.py
+++ src/client/sound/sounds.py
@@ -6,9 +6,9 @@ filepath = os.path.dirname(__file__)
 
 
 class SoundEffects(Enum):
-    PADDLE_BOUNCE = os.path.join(filepath, "original_pong_sounds/paddle_bounce.mp3")
-    WALL_BOUNCE = os.path.join(filepath, "original_pong_sounds/wall_bounce.mp3")
-    SCORE = os.path.join(filepath, "original_pong_sounds/score.mp3")
+    PADDLE_BOUNCE = os.path.join(filepath, "sound_effects/original_pong_sounds/paddle_bounce.mp3")
+    WALL_BOUNCE = os.path.join(filepath, "sound_effects/original_pong_sounds/wall_bounce.mp3")
+    SCORE = os.path.join(filepath, "sound_effects/original_pong_sounds/score.mp3")
 
 
 class Music(Enum):
