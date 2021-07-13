628572846b922a3cc4d7301b2c35b15fb50a0159
nobalpha <roni.kolukisayan@nds.k12.tr>
2021-07-13 17:01:35 +0300

    is_focused function

--- src/client/input/active_window/__init__.py
+++ src/client/input/active_window/__init__.py
@@ -1,3 +1,27 @@
 # determines whether to import linux or windows
+import platform
+import os
+import ctypes
+import time
+
+
 def is_focused():
-    return True
+    return get_window_pid(platform.system()) == init_pid
+
+
+def pid_on_start():
+    return get_window_pid(platform.system())
+
+
+def get_window_pid(system):
+    if system == "Linux":
+        return int(
+            os.popen("xprop -id `xdotool getwindowfocus` | grep '_NET_WM_PID' | grep -oE '[[:digit:]]*$'").read())
+
+    elif system == "Windows":
+        pid = ctypes.wintypes.DWORD()
+        active = ctypes.windll.user32.GetForegroundWindow()
+        return int(ctypes.windll.user32.GetWindowThreadProcessId(active, ctypes.byref(pid)))
+
+
+init_pid = pid_on_start()
