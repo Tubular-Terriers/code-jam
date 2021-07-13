70dac10a51581e6294499c153c1bb5e24d28a273
nopeless <38830903+nopeless@users.noreply.github.com>
2021-07-13 23:43:25 +0900

    migrate to pynput, add text support

--- src/client/input/input_manager.py
+++ src/client/input/input_manager.py
@@ -1,10 +1,23 @@
 # input manager imports keyboard.py
+from types import SimpleNamespace
 from typing import Optional
 
-import keyboard
+from pynput import keyboard
 
 from .active_window import is_focused
 from .state import InputState
+
+
+def is_typeable_char(key):
+    try:
+        key.char
+        return True
+    except AttributeError:
+        return False
+
+
+def get_char_of_key(key):
+    return key.char
 
 
 class InputManager:
@@ -14,7 +27,7 @@ class InputManager:
     Forwards the api
     """
 
-    def __init__(self):
+    def __init__(self, allow_text_mode=True):
         """
         Creates an InputManager that emits events
 
@@ -22,13 +35,57 @@ class InputManager:
         """
         self.text = None
         self.state = InputState.GAMEPAD
+        self.last_state = self.state
 
-    def is_pressed(self, key):
-        if not is_focused():
-            return False
-        if self.state == InputState.TYPING:
-            return False
-        return keyboard.is_pressed(key)
+        self.callback = SimpleNamespace()
+        self.callback.on_press = lambda _: None
+        self.callback.on_release = lambda _: None
+        self.callback.on_text_start = lambda _: None
+        self.callback.on_text_update = lambda _: None
+        self.callback.on_text_end = lambda _: None
+
+        on_press = None
+        text_manager = None
+
+        if allow_text_mode:
+            # Text callback methods are managed here
+            # this is the only place where self.text is changed (except for self.set_text)
+            def text_manager(k):
+                if self.text is None:
+                    self.text = []
+                    self.callback.on_text_start(self.get_text())
+                if is_typeable_char(k):
+                    self.text.append(get_char_of_key(k))
+                    self.callback.on_text_update(self.get_text())
+                elif k == keyboard.Key.enter:
+                    self.callback.on_text_end(self.get_text())
+                    self.exit_input()
+
+            def on_press(k):
+                if not is_focused():
+                    return
+                if self.state == InputState.TYPING:
+                    text_manager(k)
+                else:
+                    self.callback.on_press(k)
+
+        else:
+
+            def on_press(k):
+                if not is_focused():
+                    return
+                self.callback.on_press(k)
+
+        def on_release(k):
+            if not is_focused():
+                return
+            self.callback.on_release(k)
+
+        keyboard.Listener(on_press=on_press, on_release=on_release).start()
+
+    #############################################
+    # Call back methods
+    #############################################
 
     def hook(self, *args):
         """`keyboard.hook` Raw hook"""
@@ -39,25 +96,59 @@ class InputManager:
         """`keyboard.unhook`"""
         raise NotImplementedError
         # return keyboard.unhook(*args)
+
+    def on_press(self, cb):
+        self.callback.on_press = cb
+
+    def on_release(self, cb):
+        self.callback.on_release = cb
+
+    def on_text_start(self, cb):
+        self.callback.on_text_start = cb
+
+    def on_text_update(self, cb):
+        self.callback.on_text_update = cb
+
+    def on_text_end(self, cb):
+        """Supplies an"""
+        self.callback.on_text_end = cb
+
+    ###############################################
+    # Normal methods
+    ###############################################
+
+    def trigger_input(self):
+        self.state = InputState.TYPING
+
+    def is_pressed(self, key):
+        if not is_focused():
+            return False
+        if self.state == InputState.TYPING:
+            return False
+        return keyboard.is_pressed(key)
+
+    def exit_input(self):
+        """Exits input mode to original mode"""
+        # this shouldn't happen
+        if self.last_state == InputState.TYPING:
+            self.state = InputState.GAMEPAD
+            return
+        self.last_state, self.state = self.state, self.last_state
+
     def set_state(self, state: InputState) -> None:
         """Has to be one of the states"""
+        if self.state == state:
+            return
+        self.last_state = self.state
         self.state = state
 
     def get_text(self) -> Optional[str]:
         if self.text:
-            return self.text
+            return "".join(self.text)
         return None
 
-    def hook(self, *args):
-        """`keyboard.hook`"""
-        return keyboard.hook(*args)
-
-    def unhook(self, *args):
-        """`keyboard.unhook`"""
-        return keyboard.unhook(*args)
-
-    def on_press(self, cb):
-        return keyboard.on_press(cb)
+    def set_text(self, s: str):
+        self.text = s
 
 
 input_manager = InputManager()
