8ff1af6412fac3f297265b450591bfa9202b2bfb
Lint Action <lint-action@samuelmeuli.com>
2021-07-13 11:35:44 +0000

    Fix code style issues with Black

--- src/client/rich_presence/rich_presence.py
+++ src/client/rich_presence/rich_presence.py
@@ -16,37 +16,52 @@ class RichPresence:
         self._client_id_ = environ.get("client_id")
         self._rich_presence_client_ = Client(self._client_id_)
         self.state = RichPresenceState.DISCONNECTED
-        
+
     def start_connection(self):
         self._rich_presence_client_.start()
         self.state = RichPresenceState.CONNECTED
-        
+
     def end_connection(self):
         self._rich_presence_client_.close()
         self.state = RichPresenceState.DISCONNECTED
 
-    def update_activity(self, pid: int = getpid(),
-                        state: str = None, details: str = None,
-                        start: int = None, end: int = None,
-                        large_image: str = None, large_text: str = None,
-                        small_image: str = None, small_text: str = None,
-                        party_id: str = None, party_size: list = None,
-                        join: str = None, spectate: str = None,
-                        match: str = None, buttons: list = None,
-                        instance: bool = True):
-        return self._rich_presence_client_.set_activity(pid, state, details,
-                                                        start, end,
-                                                        large_image,
-                                                        large_text,
-                                                        small_image,
-                                                        small_text,
-                                                        party_id,
-                                                        party_size,
-                                                        join,
-                                                        spectate,
-                                                        match,
-                                                        buttons,
-                                                        instance)
+    def update_activity(
+        self,
+        pid: int = getpid(),
+        state: str = None,
+        details: str = None,
+        start: int = None,
+        end: int = None,
+        large_image: str = None,
+        large_text: str = None,
+        small_image: str = None,
+        small_text: str = None,
+        party_id: str = None,
+        party_size: list = None,
+        join: str = None,
+        spectate: str = None,
+        match: str = None,
+        buttons: list = None,
+        instance: bool = True,
+    ):
+        return self._rich_presence_client_.set_activity(
+            pid,
+            state,
+            details,
+            start,
+            end,
+            large_image,
+            large_text,
+            small_image,
+            small_text,
+            party_id,
+            party_size,
+            join,
+            spectate,
+            match,
+            buttons,
+            instance,
+        )
 
     def clear_activity(self, pid: int = getpid()):
         self._rich_presence_client_.clear_activity(pid)
@@ -59,6 +74,6 @@ if __name__ == "__main__":
     r = RichPresence()
     r.start_connection()
     r.update_activity(state="dsasd")
-    
+
     while True:
         time.sleep(15)
