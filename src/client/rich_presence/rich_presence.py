8e0ef4b035cc721ad5b9e8fb7e47c10b276644ce
Me-Phew <Me-Phew@users.noreply.github.com>
2021-07-13 18:33:32 +0000

    Isort auto commit

--- src/client/rich_presence/rich_presence.py
+++ src/client/rich_presence/rich_presence.py
@@ -1,9 +1,10 @@
 import time
 from enum import Enum
 from os import environ, getpid
+
+from __static__ import *
 from dotenv import load_dotenv
 from pypresence import Client
-from __static__ import *
 
 
 class RichPresenceState(Enum):
