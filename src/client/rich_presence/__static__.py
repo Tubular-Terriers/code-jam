219a971e5f992d72655584a8c48391273ce56930
MePhew <basmateusz@wp.pl>
2021-07-13 20:39:17 +0200

    Add game state details enum for rich presence system

--- src/client/rich_presence/__static__.py
+++ src/client/rich_presence/__static__.py
@@ -1,1 +1,12 @@
 # Write static prestes for rich presence here
+
+from enum import Enum
+
+
+class RichPresenceDetails(Enum):
+    MAIN_MENU = "In main menu"
+    LOADING = "Joining server"
+    PLAYING = "In game"
+    PAUSED = "Game paused"
+    GAME_OVER = "Game lost"
+    GAME_WON = "Game won"
--- src/client/rich_presence/rich_presence.py
+++ src/client/rich_presence/rich_presence.py
@@ -3,7 +3,7 @@ from enum import Enum
 from os import environ, getpid
 from dotenv import load_dotenv
 from pypresence import Client
-from __static__ import *
+from __static__ import RichPresenceDetails
 
 
 class RichPresenceState(Enum):
