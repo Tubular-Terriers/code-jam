99c61f0d5964bc66657e1d3416cabd9744ef4dfd
madara uchiha <pritanshsahsani@gmail.com>
2021-07-14 12:57:22 +0530

    basic ui screen

---
+++ src/client/ui/game_over.py
@@ -0,0 +1,23 @@
+import curses
+from enum import Enum
+
+import time
+
+import keyboard
+
+class ProgressBar():
+    def __init__(
+        
+    ):
+        super().__init__("progress bar")
+    
+    def refresh(self):
+        
+    def set_progress(self, progress: float):
+        
+    def _create_message_(self) -> str:
+        
+
+if __name__ == "__main__":
+    curses.initscr()
+    
