7c7acb4a1c07e0b3bb347af163105aa31be9b5e4
Nickhil1737 <Nickhil1737@users.noreply.github.com>
2021-07-14 13:00:01 +0000

    Isort auto commit

--- src/website/dcauth/views.py
+++ src/website/dcauth/views.py
@@ -1,12 +1,9 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import redirect, render
+from django.urls import reverse
+from django.views import View
 from django.views.generic.base import TemplateView
 from django.views.generic.edit import FormView
-from django.views import View
-from django.http import HttpResponseRedirect
-from django.shortcuts import render
-from django.shortcuts import redirect
-from django.urls import reverse
-
 
 gameserver_url = "https://pongconsole.xyz/"
 discord_auth_url = "https://discord.com/api/oauth2/authorize?client_id=864848740387782666&redirect_uri=https%3A%2F%2Fpongconsole.xyz%2F&response_type=code&scope=identify"
