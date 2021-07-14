5be852896bd33c1704913497a83fd3806e4701dd
Lint Action <lint-action@samuelmeuli.com>
2021-07-14 17:45:10 +0000

    Fix code style issues with Black

--- src/website/codejamtt/urls.py
+++ src/website/codejamtt/urls.py
@@ -17,21 +17,20 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
 ]
 
 from django.urls import include
 
 urlpatterns += [
-    path('dcauth/', include('dcauth.urls')),
+    path("dcauth/", include("dcauth.urls")),
 ]
 from django.views.generic import RedirectView
+
 urlpatterns += [
-    path('', RedirectView.as_view(url='dcauth/', permanent=True)),
+    path("", RedirectView.as_view(url="dcauth/", permanent=True)),
 ]
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-
--- src/website/codejamtt/wsgi.py
+++ src/website/codejamtt/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'codejamtt.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "codejamtt.settings")
 
 application = get_wsgi_application()
