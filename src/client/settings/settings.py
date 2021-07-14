485064413b0eb7151a070c2f40ab9e935cf56812
Lint Action <lint-action@samuelmeuli.com>
2021-07-14 23:18:59 +0000

    Fix code style issues with Black

--- src/client/settings/settings.py
+++ src/client/settings/settings.py
@@ -9,15 +9,15 @@ class Settings:
         self._config_ = {}
         for v in self._configparser_["USER"]:
             self._config_[v] = self._configparser_["USER"][v]
-        
+
     def update_value(self, key, value):
         # self.config[key] = value # TODO: Add this
         pass
-    
+
     def get_value(self, value, fallback_value):
         return self._config_.get(value, fallback_value)
-        
-        
+
+
 if __name__ == "__main__":
     settings = Settings()
     print(settings.get_value("2", "4"))
