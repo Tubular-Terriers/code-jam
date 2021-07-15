ee3d3041c78d55e583c7e8e075432df183661b58
nopeless <38830903+nopeless@users.noreply.github.com>
2021-07-16 05:13:06 +0900

    was conflicting with keymappings

--- src/game/events/move_bar.py
+++ src/game/events/move_bar.py
@@ -2,11 +2,11 @@ from ._event import Event
 
 
 class MoveBar(Event):
-    UP = "UP"
-    DOWN = "DOWN"
-    LEFT = "LEFT"
-    RIGHT = "RIGHT"
-    ID = "MOVE_BAR"
+    UP = "BAR_UP"
+    DOWN = "BAR_DOWN"
+    LEFT = "BAR_LEFT"
+    RIGHT = "BAR_RIGHT"
+    ID = "BAR_MOVE_BAR"
 
     def __init__(self, value):
         super().__init__(self.ID, value)
