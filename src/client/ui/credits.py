e7cac86982dc8e8777f5d6b3f9cd75354054e249
MePhew <basmateusz@wp.pl>
2021-07-15 23:03:55 +0200

    Improved settings a bit

--- src/client/ui/credits.py
+++ src/client/ui/credits.py
@@ -1,5 +1,6 @@
 import asyncio
 import curses
+import random
 import time
 from time import sleep
 
@@ -16,79 +17,72 @@ from .widget.simple_textbox import Box
 class Credits(UI):
     def __init__(self):
         super().__init__("main_menu_scr")
-
-        # fmt: off
-        self.message_text1 = "                          _  _    _          "  # noqa: W605
-        self.message_text2 = "  ____  _____   ____   __| ||_| _| |_   ___ "  # noqa: W605
-        self.message_text3 = " /  __||_  __\_/ __ \ / _  || ||_   _|/  __/"  # noqa: W605
-        self.message_text4 = " | |__ |  | \/\  ___// |_| || |  | |  \___ \ "  # noqa: W605
-        self.message_text5 = " \____||__|    \____||_____||_|  |_|  /____/"  # noqa: W605
-        # fmt: on
-        self.message_text6 = ""  # nopeless
-        self.message_text7 = ""  # karthikmurakonda
-        self.message_text8 = ""  # MePhew
-        self.message_text9 = ""  # pritansh sahsani
-        self.message_text10 = ""  # nobalpha
-        self.message_text11 = ""  # Nickhil1737
-
-        # print(self.message)
+        self.stars = ["✶", "*"]
+        self.spacing = 3
+        self.button_text = "Press Space to go to back to game menu"
+        self._credits_ = [
+            "                          _  _    _          ",
+            "  ____  _____   ____   __| ||_| _| |_   ___ ",
+            " /  __||_  __\_/ __ \ / _  || ||_   _|/  __/",
+            " | |__ |  | \/\  ___// |_| || |  | |  \___ \ ",
+            " \____||__|    \____||_____||_|  |_|  /____/"
+        ]
+        self.devs = ["nopeless", "karthikmurakonda", "pritansh sahsani", "MePhew", "nobalpha", "Nickhil1737"]
 
     async def view(self, app):
         # Required
         super().view(app)
         height, width = self.window.getmaxyx()
+        print(height, width)
         curses.start_color()
         curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)
         curses.init_pair(2, curses.COLOR_BLUE, curses.COLOR_BLACK)
+        curses.init_pair(3, curses.COLOR_CYAN, curses.COLOR_BLACK)
+        curses.init_pair(4, curses.COLOR_MAGENTA, curses.COLOR_BLACK)
         self.window.attron(curses.color_pair(2))
         self.window.attron(curses.A_BOLD)
-        self.window.addstr(
-            int((height / 2) - 7),
-            int(width / 2) - 20,
-            f"{self.message_text1}",
-        )
-        self.window.addstr(
-            int((height / 2) - 6), int(width / 2) - 20, f"{self.message_text2}"
-        )
-        self.window.addstr(
-            int((height / 2) - 5), int(width / 2) - 20, f"{self.message_text3}"
-        )
-        self.window.addstr(
-            int((height / 2) - 4), int(width / 2) - 20, f"{self.message_text4}"
-        )
-        self.window.addstr(
-            int((height / 2) - 3), int(width / 2) - 20, f"{self.message_text5}"
-        )
-        self.window.addstr(
-            int((height / 2) - 2), int(width / 2) - 20, f"{self.message_text6}"
-        )
-        self.window.addstr(
-            int((height / 2) - 1), int(width / 2) - 20, f"{self.message_text7}"
-        )
-        self.window.addstr(
-            int((height / 2)), int(width / 2) - 20, f"{self.message_text8}"
-        )
-        self.window.addstr(
-            int((height / 2) + 1), int(width / 2) - 20, f"{self.message_text9}"
-        )
-        self.window.addstr(
-            int((height / 2) + 2), int(width / 2) - 20, f"{self.message_text10}"
-        )
-        self.window.addstr(
-            int((height / 2) + 3), int(width / 2) - 20, f"{self.message_text11}"
-        )
+
+        y = 0
+        for text in self._credits_:
+            self.window.addstr(height // 3 + y, width // 2 - len(text) // 2, text)
+            y += 1
+ 
+        self.window.attron(curses.A_ITALIC)
+      
+        color_id = 1
+        for nick in self.devs:
+            
+            if color_id % 2 == 0:
+                self.window.attron(curses.color_pair(3))
+            else:
+                self.window.attron(curses.color_pair(4))
+            
+            self.window.addstr(height // 3 + y + self.spacing - 1,
+                               width // 2 - len(nick) // 2,
+                               nick)
+            y += 2
+            color_id += 1
+
         self.window.attroff(curses.color_pair(2))
         self.window.attroff(curses.A_BOLD)
         self.refresh()
+
         menu_button = Button(
-            (height // 2) + 5,
-            (width // 2) - 20,
+            height // 3 + y + self.spacing - 1,
+            (width // 2) - len(self.button_text) // 2 - 3,
             key=keyboard.Key.space,
             go_to=AppState.MAIN_MENU,
         )
         self.window.addstr(
-            (height // 2) + 6, (width // 2) - 17, "Press Space to go to Game menu"
+            height // 3 + y + self.spacing,
+            (width // 2) - len(self.button_text) // 2,
+            self.button_text
         )
+        
+        for _ in range(250):
+            self.window.addstr(random.randint(0, height - 1),
+                               random.randint(0, width - 1),
+                               random.choice(self.stars))
 
         self.input_manager = app.input_manager
         self.widgets = [menu_button]
