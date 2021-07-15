c644735b00302ee376baf1998493d7344afbc606
nopeless <38830903+nopeless@users.noreply.github.com>
2021-07-16 02:39:45 +0900

    create collision types for Shape.collision_type

---
+++ src/game/collision_type.py
@@ -0,0 +1,6 @@
+# fmt: off
+WALL               = 1  # noqa: E221
+BOUNDING_BOX       = 2  # noqa: E221
+BALL               = 3  # noqa: E221
+BALL_COLLISION_BOX = 4  # noqa: E221
+HITBOX             = 5  # noqa: E221
