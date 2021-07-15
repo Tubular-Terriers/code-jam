b9f75eacea42cecc26cadcccdc6b1cbea4dcdc93
nopeless <38830903+nopeless@users.noreply.github.com>
2021-07-16 00:06:54 +0900

    add interface methods

--- src/game/entities/entity.py
+++ src/game/entities/entity.py
@@ -7,3 +7,10 @@ class Entity:
     def __init__(self, type: EntityType, uuid):
         self.uuid = uuid if uuid else uuid4()
         self.type = type
+
+    def load_data(self, data):
+        print(data)
+        raise NotImplementedError
+
+    def dump_data(self):
+        raise NotImplementedError
