579f25d49a217348eaba225375a87803ce3c4a7d
MePhew <basmateusz@wp.pl>
2021-07-15 14:52:53 +0200

    Improve user settings system

---
+++ src/client/settings/config.py
@@ -0,0 +1,42 @@
+import configparser
+from settings import Settings
+
+
+class Config:
+    def __init__(self, config_file_path: str = "config.ini"):
+        self._config_file_path_ = config_file_path
+        self._configparser_ = configparser.ConfigParser()
+        self._config_files_ = self._configparser_.read(self._config_file_path_)
+
+    def update(self, setting: Settings, value) -> None:
+        self._configparser_["USER"][setting.value] = str(value)
+        with open(self._config_file_path_, "w") as config_file:
+            self._configparser_.write(config_file)
+
+    def get(self, setting: Settings, fallback_value=None):
+        return self._configparser_["USER"].get(setting.value, fallback_value)
+    
+    def restore_default_config(self):
+        self._configparser_["USER"].clear()
+        with open(self._config_file_path_, "w") as config_file:
+            self._configparser_.write(config_file)
+
+
+if __name__ == "__main__":
+    config = Config()
+    
+    print(config.get(Settings.MUSIC_VOLUME))
+    
+    config.update(Settings.MUSIC_VOLUME, "20")
+    
+    print(config.get(Settings.MUSIC_VOLUME))
+    
+    config.restore_default_config()
+
+    print(config.get(Settings.MUSIC_VOLUME))
+    
+    config.update(Settings.MUTE_GAME_ON_FOCUS_LOSS, True)
+    config.update(Settings.MUSIC_VOLUME, 50)
+    config.update(Settings.SFX_VOLUME, 20)
+    config.update(Settings.DECREASE_VOLUME_DURING_GAME_PAUSE, False)
+    config.update(Settings.GAME_PAUSE_NORMAL_VOLUME_PERCENTAGE, 70)
--- src/client/settings/settings.py
+++ src/client/settings/settings.py
@@ -1,23 +1,9 @@
-import configparser
+from enum import Enum
 
 
-class Settings:
-    def __init__(self, config_file_path: str = "config.ini"):
-        self._configparser_ = configparser.ConfigParser()
-        self._config_file_name_path_ = config_file_path
-        self._parsed_config_ = self._configparser_.read(self._config_file_name_path_)
-        self._config_ = {}
-        for v in self._configparser_["USER"]:
-            self._config_[v] = self._configparser_["USER"][v]
-
-    def update_value(self, key, value):
-        # self.config[key] = value # TODO: Add this
-        pass
-
-    def get_value(self, value, fallback_value):
-        return self._config_.get(value, fallback_value)
-
-
-if __name__ == "__main__":
-    settings = Settings()
-    print(settings.get_value("2", "4"))
+class Settings(Enum):
+    MUSIC_VOLUME = "music_volume"
+    SFX_VOLUME = "sfx_volume"
+    DECREASE_VOLUME_DURING_GAME_PAUSE = "decrease_volume_during_game_pause"
+    GAME_PAUSE_NORMAL_VOLUME_PERCENTAGE = "game_pause_normal_volume_percentage"
+    MUTE_GAME_ON_FOCUS_LOSS = "mute_game_on_focus_loss"
