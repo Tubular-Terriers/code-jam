f966199c2cb4f9481b38a68dc007b78d974b8608
Nickhil1737 <58224547+Nickhil1737@users.noreply.github.com>
2021-07-15 22:51:11 +0530

    fixes

---
+++ src/website/dcauth/views.py
@@ -0,0 +1,28 @@
+from urllib import parse
+
+from django.http import HttpResponseRedirect
+from django.shortcuts import redirect, render
+from django.urls import reverse
+from django.views import View
+from django.views.generic.base import TemplateView
+from django.views.generic.edit import FormView
+
+gameserver_url = "https://pongconsole.xyz/"
+
+client_id = "864835657091252234"
+redirect_uri = parse(f"{gameserver_url}login-success")
+
+discord_auth_url = (
+    "https://discord.com/api/oauth2/authorize?"
+    + f"client_id={client_id}&redirect_uri={redirect_uri}&response_type=code&scope=identify"
+)
+
+
+class todoHome(TemplateView):
+    def get(self, request, *args, **kwargs):
+
+        return render(request, "index.html")
+
+
+def login_via_discord(request):
+    return redirect(discord_auth_url)
