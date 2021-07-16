205dde7b66ec020400ce11fc9dff5f19dc8e4e9d
nopeless <38830903+nopeless@users.noreply.github.com>
2021-07-16 21:20:43 +0900

    refine packet class

--- src/packet/_packet.py
+++ src/packet/_packet.py
@@ -1,14 +1,26 @@
-class Packet:
-    """Interface class"""
+import json
 
-    def __init__(self):
-        pass
+
+class Packet:
+    """Interface class. Only send() creates a packet with action name"""
+
+    def __init__(self, action_name: str):
+        self.action_name = action_name
 
     def dump(self):
         """Dumps the data to payload json form"""
         raise NotImplementedError
 
     @staticmethod
-    def load(data):
+    def load(data) -> object:
         """Loads the data from payload json"""
         raise NotImplementedError
+
+    def send(self) -> str:
+        """Returns a json packet to send"""
+        json.dumps(
+            {
+                "action": self.action_name,
+                "payload": self.dump(),
+            }  # self.dump() must be implemented
+        )
