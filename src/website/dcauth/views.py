cb25b6cb6c8d2cb06fc0c5168d0d26cc3082b1b6
nobalpha <roni.kolukisayan@nds.k12.tr>
2021-07-16 16:33:44 +0300

    creds, auth_manager, dcauth

--- src/website/dcauth/views.py
+++ src/website/dcauth/views.py
@@ -1,3 +1,9 @@
+import requests
+from dotenv import load_dotenv
+import hashlib
+import os
+from .auth_manager import AuthManager
+
 from urllib import parse
 
 from django.http import HttpResponseRedirect
@@ -7,15 +13,15 @@ from django.views import View
 from django.views.generic.base import TemplateView
 from django.views.generic.edit import FormView
 
-gameserver_url = "https://pongconsole.xyz/"
+load_dotenv()
 
-client_id = "864835657091252234"
-redirect_uri = parse.quote(f"{gameserver_url}login-success", safe="")
+GAMESERVER_URL = "https://pongconsole.xyz/dcauth/"
+DEBUG_URL = "http://127.0.0.1:8000/dcauth/"
 
-discord_auth_url = (
-    "https://discord.com/api/oauth2/authorize?"
-    + f"client_id={client_id}&redirect_uri={redirect_uri}&response_type=code&scope=identify"
-)
+CLIENT_ID = os.environ.get("CLIENT_ID")
+CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
+
+authmng = AuthManager()
 
 
 class todoHome(TemplateView):
@@ -25,4 +31,50 @@ class todoHome(TemplateView):
 
 
 def login_via_discord(request):
+    REDIRECT_URI = f"{GAMESERVER_URL}login-success"
+
+    discord_auth_url = (
+        "https://discord.com/api/oauth2/authorize?"
+        + f"client_id={CLIENT_ID}&redirect_uri={parse.quote(REDIRECT_URI)}&response_type=code&scope=identify"
+    )
     return redirect(discord_auth_url)
+
+
+def login_success(request):
+    if request.GET["code"] is not None:
+        code = request.GET["code"]
+        access_token = get_access_token(code)
+        uid = get_user_id(access_token)
+        authmng.add(uid)
+    return render(request, "token.html", {"uid": authmng.get(uid)})
+
+
+# Tools
+def get_access_token(code):
+    API_ENDPOINT = 'https://discord.com/api'
+    REDIRECT_URI = f"{GAMESERVER_URL}login-success"
+    data = {
+        "client_id": CLIENT_ID,
+        "client_secret": CLIENT_SECRET,
+        "grant_type": "authorization_code",
+        "code": code,
+        "redirect_uri": REDIRECT_URI
+    }
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+    r = requests.post(f"{API_ENDPOINT}/oauth2/token", data=parse.urlencode(data), headers=headers)
+    r.raise_for_status()
+    response = r.json()
+    return response["access_token"]
+
+
+def get_user_id(access_token):
+    API_ENDPOINT = 'https://discord.com/api'
+    headers = {
+        "Authorization": f"Bearer {access_token}"
+    }
+    r = requests.get(f"{API_ENDPOINT}/users/@me", headers=headers)
+    r.raise_for_status()
+    res = r.json()
+    return res["id"]
