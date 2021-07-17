b048e2e22bb7235ef850c8deceb25dd18c7caaf7
Me-Phew <Me-Phew@users.noreply.github.com>
2021-07-17 12:00:38 +0000

    Isort auto commit

--- src/client/ui/settings.py
+++ src/client/ui/settings.py
@@ -1,8 +1,11 @@
 import asyncio
 import curses
 import random
+
 from pynput import keyboard
+
 from client.appstate import AppState
+
 from ._ui import UI
 from .widget.button import Button
 from .widget.progress_bar import ProgressBar
