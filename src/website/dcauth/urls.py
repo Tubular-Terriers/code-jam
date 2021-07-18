bce16c852d4831ae50777e71cee7f38c819a9edf
Nickhil1737 <58224547+Nickhil1737@users.noreply.github.com>
2021-07-18 15:13:47 +0530

    fixed app url

--- src/website/dcauth/urls.py
+++ src/website/dcauth/urls.py
@@ -7,6 +7,5 @@ from .views import todoHome
 urlpatterns = [
     path("", todoHome.as_view(), name="todoHome"),
     path("login/", views.login_via_discord, name="logindiscord"),
-    path("login-success/", views.login_success, name="loginsuccess"),
-    # path('login/redirect',views.login_redirect,name='loginredirect'),
+    path("login/success/", views.login_success, name="loginsuccess"),
 ]
